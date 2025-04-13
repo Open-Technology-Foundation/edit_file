2236581afb2e2dc5ab67a4b483dde535f2cfe6cf
Gary Dean <garydean@okusi.id>
2025-04-13 09:19:45 +0800

    update 0.9.0

--- bash_template.sh
+++ bash_template.sh
@@ -1,30 +1,43 @@
 #!/usr/bin/env bash
 #shellcheck disable=SC1091,SC2155
-set -euo pipefail
+set -eEuo pipefail +o histexpand
+shopt -s extglob globstar checkwinsize #-u dotglob nullglob
+# Ensure consistent locale and Disable history
+export LC_ALL=C HISTFILE=/dev/null HISTSIZE=0
+
 readonly -- VERSION='1.0.0'
 readonly -- PRG0="$(readlink -en -- "$0")"
 #shellcheck disable=SC2034
 readonly -- PRGDIR="${PRG0%/*}" PRG="${PRG0##*/}"
+
+# Message functions for formatting output
 declare -ix VERBOSE=1 DEBUG=0 DRYRUN=1
+declare -- RED='' YELLOW='' GREEN='' CYAN='' NOCOLOR=''
+[ -t 2 ] && { RED=$'\e[31m' YELLOW=$'\e[33m' GREEN=$'\e[32m' CYAN=$'\e[36m' NOCOLOR=$'\e[0m'; }
 #shellcheck disable=SC2015
-[ -t 2 ] && declare -- RED=$'\033[0;31m' YELLOW=$'\033[0;33m' GREEN=$'\033[0;32m' NOCOLOR=$'\033[0m' || declare -- RED='' YELLOW='' GREEN='' NOCOLOR=''
-vecho() { ((VERBOSE)) || return 0; local msg; for msg in "$@"; do printf '%s: %s\n' "$PRG" "$msg"; done; }
-vinfo() { ((VERBOSE)) || return 0; local msg; for msg in "$@"; do >&2 printf '%s: %sinfo%s: %s\n' "$PRG" "$GREEN" "$NOCOLOR" "$msg"; done; }
-vwarn() { ((VERBOSE)) || return 0; local msg; for msg in "$@"; do >&2 printf '%s: %swarn%s: %s\n' "$PRG" "$YELLOW" "$NOCOLOR" "$msg"; done; }
-vdebug(){ local msg; for msg in "$@"; do >&2 printf '%s: %sdebug%s: %s\n' "$PRG" "$YELLOW" "$NOCOLOR" "$msg"; done; }
-error() { local msg; for msg in "$@"; do >&2 printf '%s: %serror%s: %s\n' "$PRG" "$RED" "$NOCOLOR" "$msg"; done; }
-die() { local -i exitcode=1; if (($#)); then exitcode=$1; shift; fi; if (($#)); then error "$@"; fi; exit "$exitcode"; }
+_text() { (($1)) && ((${#3})) && printf '%s%s: %s%s\n' "$2" "$PRG" "$3" "$NOCOLOR" || true; }
+text()    {     _text 1        ""         "$*"; }
+success() {     _text $VERBOSE "$GREEN"   "$*"; }
+info()    { >&2 _text $VERBOSE ""         "$*"; }
+warn()    { >&2 _text $VERBOSE "$YELLOW"  "$*"; }
+error()   { >&2 _text 1        "$RED"     "$*"; }
+debug()   { >&2 _text $DEBUG   "$CYAN"    "$*"; }
+die() { error "${2:-}"; exit "${1:-1}"; }
+
 grep() { /usr/bin/grep "$@"; }
 find() { /usr/bin/find "$@"; }
 rsync() { /usr/bin/rsync "$@"; }
 ssh() { /usr/bin/ssh "$@"; }
 scp() { /usr/bin/scp "$@"; }
 sed() { /usr/bin/sed "$@"; }
-declare -fx grep find rsync ssh scp
+declare -fx grep find rsync ssh scp sed
+
 noarg() { if (($# < 2)) || [[ ${2:0:1} == '-' ]]; then die 2 "Missing argument for option '$1'"; fi; true; }
 decp() { declare -p "$@" | sed 's/^declare -[a-zA-Z-]* //'; }
+
 xcleanup() { local -i exitcode=${1:-0}; [[ -t 0 ]] && printf '\e[?25h'; exit "$exitcode"; }
 trap 'xcleanup $?' SIGINT EXIT
+
 # Requires root or sudo
 #((EUID)) && { sudo -ln &>/dev/null || die 1 "Requires root, or non-interactive sudo privileges."; exec sudo -n "$0" "$@"; exit 1; }
 # Requires host okusi
@@ -32,7 +45,7 @@ trap 'xcleanup $?' SIGINT EXIT
 #trim() { local v="$*"; v="${v#"${v%%[![:blank:]]*}"}"; echo -n "${v%"${v##*[![:blank:]]}"}"; }
 #remblanks() { local str="$1"; while [[ "$str" =~ ([[:space:]]{2,}) ]]; do str="${str//${BASH_REMATCH[1]}/ }"; done; trim "$str"; }
 #isodate() { date +'%Y-%m-%d %H:%M:%S'; }
-#fqcommand() { local command="$1"; local apt_package="${2:-}"; command -v "$command" &>/dev/null && { command -v "$command"; return 0; }; [[ -z "$apt_package" ]] && die 1 "Command '$command' not found and no package specified"; vinfo "Installing package: $apt_package"; sudo apt-get install -y "$apt_package" >/dev/null 2>&1; command -v "$command" &>/dev/null || die 1 "Failed to install $command"; command -v "$command"; }
+#fqcommand() { local command="$1"; local apt_package="${2:-}"; command -v "$command" &>/dev/null && { command -v "$command"; return 0; }; [[ -z "$apt_package" ]] && die 1 "Command '$command' not found and no package specified"; info "Installing package: $apt_package"; sudo apt-get install -y "$apt_package" >/dev/null 2>&1; command -v "$command" &>/dev/null || die 1 "Failed to install $command"; command -v "$command"; }
 # ----------------------------------------------------------------------------------------
 
 usage() {
@@ -45,12 +58,12 @@ Usage:
   $PRG [OPTIONS] 
 
 Options:
-	
+  
   -v, --verbose         Increase output verbosity
   -q, --quiet           Suppress non-error messages
-												$(decp VERBOSE)
+                        $(decp VERBOSE)
   -V, --version         Print version and exit
-												$(decp VERSION)
+                        $(decp VERSION)
   -h, --help            Display this help
 
 Examples:
@@ -73,12 +86,14 @@ main() {
     -h|--help) usage 0;; -v|--verbose) VERBOSE+=1;; -q|--quiet) VERBOSE=0;; -V|--version) echo "$PRG $VERSION"; exit 0;;
     -[hvqV]*) #shellcheck disable=SC2046 #split up single options
                   set -- '' $(printf -- "-%c " $(grep -o . <<<"${1:1}")) "${@:2}";;
+    --)           args+=( "$@" ); break ;;
     -*)           die 22 "Invalid option '$1'" ;;
     *)            die 2  "Invalid argument '$1'" ;;
                   #args+=( "$1" ) ;;
   esac; shift; done
   
-  
+  text "123334"
+  warn 'a warning'  
   
 }
 
